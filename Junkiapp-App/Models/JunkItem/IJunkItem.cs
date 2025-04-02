ba93ecad99666172715fbccf6772c75608a0497a
ofek.trattner <ofekikotrat@gmail.com>
2025-04-02 10:40:12 +0300

    Create App solution, project and first Basic Models project

---
+++ Junkiapp-App/Models/JunkItem/IJunkItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.JunkItem
+{
+    public interface IJunkItem
+    {
+        Guid Id { get; }
+        Tuple<double, double> Location { get; }
+        Guid ImageId { get; }
+        DateTime TimeUploaded { get; }
+    }
+}
---
+++ Junkiapp-App/Models/JunkItem/JunkItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.JunkItem
+{
+    public class JunkItem : IJunkItem
+    {
+        public Guid Id { get;  }
+
+        public Tuple<double, double> Location { get; }
+
+        public Guid ImageId { get; }
+
+        public DateTime TimeUploaded { get; }
+    }
+}
