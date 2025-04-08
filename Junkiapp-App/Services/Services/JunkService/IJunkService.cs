a74cdeabc460fdf867de3f97f4a79d37d0549b25
ofek.trattner <ofekikotrat@gmail.com>
2025-04-08 14:53:22 +0300

    Added JunkService with only Get Set features

---
+++ Junkiapp-App/Services/Services/JunkService/IJunkService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models.BasicModels;
+using Models.JunkItem;
+
+namespace Services.Services
+{
+    public interface IJunkService
+    {
+        IJunkItem GetJunkItem(Guid junkId);
+        List<Guid> GetJunkIdsByLocationAndRadius(Location location, double radius);
+        void UploadJunkItem(IJunkItem junkItem);
+        void DeleteJunkItem(Guid junkId);
+    }
+}
---
+++ Junkiapp-App/Services/Services/JunkService/JunkService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models.JunkItem;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System.Runtime.CompilerServices;
+using MongoDB.Bson.Serialization;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using Models.BasicModels;
+
+namespace Services.Services.JunkService
+{
+    public class JunkService : IJunkService
+    {
+        private MongoClient mongoClient;
+        private MySqlConnection mysqlClient;
+        private String mySqlDB = "junkdb";
+        private String mySqlJunkLocationTable = "junk_location";
+        private String mongoDBName= "JunkAppDB";
+        private String mongoJunkCollectionName = "JunkItems";
+        public IJunkItem GetJunkItem(Guid junkId)
+        {
+            var filter = Builders<IJunkItem>.Filter.Eq("Id", junkId);
+
+            IJunkItem junkItem =
+                mongoClient.GetDatabase(mongoDBName)
+                      .GetCollection<IJunkItem>(mongoJunkCollectionName)
+                      .Find(filter).First();
+
+            return junkItem;
+        }
+
+        public void UploadJunkItem(IJunkItem junkItem)
+        {
+            UploadJunkItemToMongoDB(junkItem);
+            UploadJunkLocationToSql(junkItem);
+        }
+        private void UploadJunkItemToMongoDB(IJunkItem junkItem)
+        {
+            var collection = mongoClient
+                .GetDatabase(mongoDBName)
+                .GetCollection<IJunkItem>(mongoJunkCollectionName);
+            collection.InsertOne(junkItem);
+        }
+
+        private void UploadJunkLocationToSql(IJunkItem junkItem)
+        {
+            String sqlInsertCommand = $"INSERT INTO {mySqlDB}.{mySqlJunkLocationTable}(id, location) VALUES({junkItem.Id}, " +
+                $"POINT({junkItem.Location.Longtitude}, {junkItem.Location.Latitude}))";
+            MySqlCommand cmd = new MySqlCommand(sqlInsertCommand, mysqlClient);
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
